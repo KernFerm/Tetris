54009993d907d5d2838ede0567ef14b31d390a95
bubbles <152947339+KernFerm@users.noreply.github.com>
2024-01-07 22:29:11 -0500

    Add files via upload

---
+++ PlayTetris.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Set the path to your Python interpreter
+set PYTHON="C:\Python310\python.exe"
+
+REM Set the path to your script
+set SCRIPT="tetris.py"
+
+REM Run the script
+%PYTHON% %SCRIPT%
