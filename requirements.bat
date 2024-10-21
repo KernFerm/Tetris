1241a158790f41214aa75540c04aac0267a51722
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-10-21 07:59:38 -0400

    Update requirements.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- requirements.bat
+++ requirements.bat
@@ -1,13 +1,8 @@
 @echo off
 
-echo Installing Pygame 2.6.1
-pip install pygame==2.6.1
+echo Installing Pygame
+pip install pygame
 echo Press Enter to continue with the rest of the dependencies
-pause
-
-echo Installing PyArmor
-pip install pyarmor
-echo Press Enter to continue
 pause
 
 echo All requirements installed.
